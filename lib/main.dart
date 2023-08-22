c54a98f47cc5c0a7d7852f5b4db4cbe84a74889c
Umer <mohammadumersiddiq@gmail.com>
2023-08-22 21:43:42 +0500

    3 Commit

--- lib/main.dart
+++ lib/main.dart
@@ -23,18 +23,3 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return const OnBoardingScreen1();
-  }
-}
